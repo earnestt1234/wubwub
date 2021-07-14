197e38c7e10d30b10739f22c6a95c6f74ba9f678
Tom Earnest <earnestt1234@gmail.com>
2021-07-13 22:51:14 -0500

    Create pattern.py

---
+++ wubwub/pattern.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 13 22:45:17 2021
+
+@author: earnestt1234
+"""
+
+class Pattern:
+    def __init__(self, length, pattern):
+        self.length = length
+        self.pattern = pattern
+
+    def __add__(self, other):
+        pass
