d45f2d76ad965e8fb87c257cd8a0b49176027a50
Tom Earnest <earnestt1234@gmail.com>
2021-12-26 15:10:18 -0600

    start pattern docs

--- wubwub/pattern.py
+++ wubwub/pattern.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jul 13 22:45:17 2021
-
-@author: earnestt1234
+Class for encoding rhythmic patterns.
 """
 
 from sortedcontainers import SortedList
 
 class Pattern:
-    def __init__(self, pattern, length,):
+    def __init__(self, pattern, length):
         self.pattern = SortedList(pattern)
         self.length = length
 
