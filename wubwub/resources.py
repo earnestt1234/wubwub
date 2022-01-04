68d4a9651325e0526e8a89a489ddeb73220ffde0
Tom Earnest <earnestt1234@gmail.com>
2022-01-04 11:39:25 -0600

    add resources docs

--- wubwub/resources.py
+++ wubwub/resources.py
@@ -1,24 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Feb  9 10:13:26 2021
-
-@author: earnestt1234
+General functions/constants used by wubwub.
 """
 
 import random
-
-import numpy as np
-
 
 SECOND = 1000
 MINUTE = 60 * SECOND
 
 def random_choice_generator(x):
+    '''Generate repeated random choices from `x`.'''
     while True:
         yield random.choice(x)
 
 def unique_name(base, others):
+    '''Generate a new name, checked against a list of names.'''
     c = 1
     name = base + str(c)
     while name in others:
